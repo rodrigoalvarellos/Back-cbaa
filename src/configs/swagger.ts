b5f79a0ae88c16c2d31c3a13c123d3e76dda7eaa
rodrigoalvarellos <rodri.alvarellos@gmail.com>
2019-11-04 11:30:16 -0300

    Implementacion de Swagger

---
+++ src/configs/swagger.ts
@@ -0,0 +1,14 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export function SwaggerConfig(app: INestApplication) {
+
+    const options = new DocumentBuilder()
+        .setTitle('CBA-Accesible - Backend API')
+        .setDescription('Backend desarrollado con Nest:JS. For more help go to https://docs.nestjs.com/recipes/swagger')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('swagger', app, document);
+
+}
